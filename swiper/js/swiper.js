95554090865cca38f0b88801e2bb01a2760330dc
Nirman Đulbić <67234814+Nirman452@users.noreply.github.com>
2020-11-04 19:59:17 +0100

    Visual tweaks
    -alignment of elements fixed,
    -arrows are now black,
    -other visual adjustments

--- swiper/js/swiper.js
+++ swiper/js/swiper.js
@@ -18,7 +18,7 @@ var swiperH = new Swiper('.swiper-container-h', {
 //settings for the vertical swiper
 var swiperV = new Swiper('.swiper-container-v', {
     direction: 'vertical',
-    spaceBetween: 10,
+    spaceBetween: 1,
     autoplay: {
         delay: 2500,
         disableOnInteraction: true,
